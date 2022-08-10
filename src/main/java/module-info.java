ccee27a5f68f766916f4c2491df16ac1485ba8cd
yilmazbahar <104997800+yilmazbahar@users.noreply.github.com>
2022-08-10 20:42:09 +0300

    initial commit .gitignore

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.eu9gr1git_bhr {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.eu9gr1git_bhr to javafx.fxml;
+    exports com.example.eu9gr1git_bhr;
+}
